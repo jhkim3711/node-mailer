bcdf7febb620f4ad044c7fb90c3d63eb5a829924
unknown <james.kim3711@gmail.com>
2025-12-31 12:51:31 +0900

    third update next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // 정적 HTML로 빌드
+  images: {
+    unoptimized: true // export 모드에서 필수
+  }
   /* config options here */
 };
 
