f307e29820fedecf45e621529be077d86659f302
unknown <james.kim3711@gmail.com>
2025-12-31 12:11:54 +0900

    second update postbuild

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -3,8 +3,10 @@
 rm -rf ./.amplify-hosting
  
 mkdir -p ./.amplify-hosting/compute
+mkdir -p ./.amplify-hosting/static
  
 cp -r ./src ./.amplify-hosting/compute/default
+cp package.json ./.amplify-hosting/compute/default/package.json
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
  
 cp -r public ./.amplify-hosting/static
